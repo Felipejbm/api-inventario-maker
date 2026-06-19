ed3ee13b5bb0c0a54aefa50a9f253f2ee97bc08f
FelipeJbm <felipejbmello.etec@gmail.com>
2026-06-18 21:11:03 -0300

    feat: aula 03 - adiciona novo campo de estado_conservacao

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ class ComponenteSchema(BaseModel):
     nome: str = Field(..., min_length=2, description="Nome do componente maker")
     quantidade: int = Field(..., ge=0, description="Quantidade em estoque (deve ser maior ou igual a zero)")
     categoria: str = Field(..., description="Categoria do item (ex: Atuadores, Microcontroladores)")
+    estado_conservacao: str = Field(..., description="Estado de conservação do produto")
 
 
 # 3. Nosso "Banco de Dados" temporário em memória
 estoque_laboratorio = [
-    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão"},
-    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores"},
-    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica"}
+    { "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão", "estado_conservacao": "Bom", "id": 1 },
+    { "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores", "estado_conservacao": "Mediano", "id": 2},
+    { "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica", "estado_conservacao": "Ruim", "id": 3,}
 ]
 
 
@@ -60,6 +61,7 @@ def atualizar_componente(componente_id: int, dados_atualizados: ComponenteSchema
             item["nome"] = dados_atualizados.nome
             item["quantidade"] = dados_atualizados.quantidade
             item["categoria"] = dados_atualizados.categoria
+            item["estado_conservacao"] = dados_atualizados.estado_conservacao
 
     return {"mensagem": "Componente atualizado com sucesso!", "componente": item}
     raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
@@ -69,7 +71,7 @@ def atualizar_componente(componente_id: int, dados_atualizados: ComponenteSchema
 @app.delete("/componentes/{componente_id}")
 def remover_componente(componente_id: int):
     for index, item in enumerate(estoque_laboratorio):
-        if item["id"] == componente_id: destoque_laboratorio.pop(index)
+        if item["id"] == componente_id: estoque_laboratorio.pop(index)
 
     return {"mensagem": f"Componente com ID {componente_id} foi removido do estoque."}
     raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
